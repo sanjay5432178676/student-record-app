6dfd61cd4322f2d958b0e65c63d98aaeea2dbe9b
Your Name <youremail@example.com>
2025-07-07 00:45:29 +0530

    Added missing templates

--- app.py
+++ app.py
@@ -194,5 +194,6 @@ def error():
 
 # ---------- RUN SERVER ----------
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  # Render uses dynamic port
-    app.run(debug=True, host='0.0.0.0', port=port)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=False, host='0.0.0.0', port=port)
